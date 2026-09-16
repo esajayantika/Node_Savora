9694a47e02b036dac720891de2181f30a9343ba6
I Putu Esa Jayantika <esajayantika@gmail.com>
2026-09-16 15:57:50 +0800

    feat(atlas connect)

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,11 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/esa')
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err))
+require("dotenv").config();
+
+const dns = require("dns");
+const mongoose = require("mongoose");
+// Gunakan dnscrypt-proxy sebagai DNS lokal
+dns.setServers(["127.0.0.1"]);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Atlas connected!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
